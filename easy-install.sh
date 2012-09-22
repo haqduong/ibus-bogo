a738dc40a6e0041af96882dcd1252709d89930bc
Duong Quang Ha <contact@haqduong.net>
2012-09-23 00:29:43 +0700

    Fix bug in easy-install.sh

--- easy-install.sh
+++ easy-install.sh
@@ -3,11 +3,14 @@ git submodule update
 
 # install BoGoEngine
 cd bogo
+git sumodule init
+git submodule update
 mkdir build
 cd build
 cmake ..
 make
 sudo make install
+cd ../..
 
 # install ibus-bogo-python
 cd ibus-bogo-python/
